d6477b262164ccb2c5c25b20e92e457764230876
Nicolas Raulin <nicolas.raulin@epfl.ch>
2024-01-01 04:37:31 +0100

    added checks for line completion

--- src/board.h
+++ src/board.h
@@ -15,5 +15,8 @@ struct board_state {
 	uint8_t data[BOARD_WIDTH * BOARD_HEIGHT]; 		/* 0 => empty cell, 1+ => cell containing a block */
 };
 
-inline uint8_t board_get(const struct board_state *board, int32_t width, int32_t row, int32_t col);
-inline void board_set(struct board_state *board, uint32_t width, int32_t row, int32_t col, int8_t value);
+uint8_t board_get(const struct board_state *board, int32_t width, int32_t row, int32_t col);
+void board_set(struct board_state *board, int32_t width, int32_t row, int32_t col, int8_t value);
+
+int32_t find_lines(const struct board_state *board, int32_t width, int32_t height, uint8_t *lines_out);
+void clear_lines(struct board_state *board, int32_t width, int32_t height, const uint8_t *lines);
