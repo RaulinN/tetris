4e9a70ff1cae28a4aee385c07f05284c004668b4
Nicolas Raulin <nicolas.raulin@epfl.ch>
2024-01-01 21:51:20 +0100

    added possibility to log string without additional arguments

--- src/log.h
+++ src/log.h
@@ -6,19 +6,28 @@
 
 #include <stdio.h>
 
-#define LOG_DEBUG(fmt, args...)	LOG_INTERNAL("DEBUG", fmt, args)
-#define LOG_INFO(fmt, args...)	LOG_INTERNAL("INFO", fmt, args)
-#define LOG_WARN(fmt, args...)	LOG_INTERNAL("WARN", fmt, args)
-#define LOG_ERROR(fmt, args...)	LOG_INTERNAL("ERROR", fmt, args)
+#define LOG_DEBUG(fmt, ...)	LOG_INTERNAL("DEBUG", fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...)	LOG_INTERNAL("INFO", fmt, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...)	LOG_INTERNAL("WARN", fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...)	LOG_INTERNAL("ERROR", fmt, ##__VA_ARGS__)
+
+#define FLOG_DEBUG(fmt, ...) FLOG_INTERNAL("DEBUG", fmt, ##__VA_ARGS__)
+#define FLOG_INFO(fmt, ...)	 FLOG_INTERNAL("INFO", fmt, ##__VA_ARGS__)
+#define FLOG_WARN(fmt, ...)	 FLOG_INTERNAL("WARN", fmt, ##__VA_ARGS__)
+#define FLOG_ERROR(fmt, ...) FLOG_INTERNAL("ERROR", fmt, ##__VA_ARGS__)
+
+
+#define FLOG_INTERNAL(level, fmt, ...)	                                                		\
+	do {                                                                                    	\
+        fprintf(                                                                            	\
+			stderr,                                                                         	\
+			"[" level "] %s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__    \
+		); 																						\
+	} while (0)
 
 #if defined(DEBUG) && DEBUG > 0
-#define LOG_INTERNAL(level, fmt, args...)	                                                \
-	do {                                                                                    \
-        fprintf(                                                                            \
-			stderr,                                                                         \
-			"[" level "] %s:%d:%s(): " fmt "\n", __FILE__, __LINE__, __func__, ##args       \
-		); 																					\
-	} while (0)
+#define LOG_INTERNAL(level, fmt, ...)	                                                		\
+	do { FLOG_INTERNAL(level, fmt, ##__VA_ARGS__); } while (0)
 #else
-#define LOG_INTERNAL(level, fmt, args...) do {} while (0) /* Don't print in release */
+#define LOG_INTERNAL(level, fmt, ...) do {} while (0) /* Don't print in release */
 #endif
